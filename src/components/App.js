050d42ac66d19c2254ec8319e7d87748c3222299
Rahulllkr <rahulkumar8604745@gmail.com>
2025-11-04 16:54:23 +0530

    check now

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,38 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const [fields,setfields] = useState([
+    {name:"",age:""}
+  ])
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name,age)
+  }
+  const handleRemove = () => {
+    
+  }
+  const handleAdd = () => {
+
+  }
+  const handleSubmit = () => {
+
+  }
   return (
     <div>
         {/* Do not remove the main div */}
-        <h1>hello</h1>
+        <form onClick={handleSubmit}>
+          <input type="text" placeholder="Name"/>
+          <input type="text" placeholder="Age"/>
+          <button onClick={handleRemove}>remove</button>
+          <button onClick={handleAdd}>Add More</button>
+          <button onClick={handleSubmit}>Submit</button>
+        </form>
+
+        
     </div>
   )
 }
